30ec751e804b1b609db71c75e0752e0ade63e2a4
Dpmqueiroz <danielqueiroz94@gmail.com>
2021-07-22 19:09:40 -0300

    Exercicio de Calendário

---
+++ Orientação a Objeto com Java e Calendar/src/main/java/aulas/trabalhandoComDatas/javaCalendar/ExercicioFinal.java
@@ -0,0 +1,19 @@
+package aulas.trabalhandoComDatas.javaCalendar;
+
+import java.util.Calendar;
+
+public class ExercicioFinal {
+    public static void main(String[] args) {
+
+        Calendar dataHoje = Calendar.getInstance();
+        System.out.println(dataHoje.getTime());
+        dataHoje.add(Calendar.DATE, 9);
+        System.out.println("A data de vencimento deveria ser: " + dataHoje.getTime());
+
+        while(dataHoje.getTime().getDay() == 0 || dataHoje.getTime().getDay() == 6){
+            dataHoje.add(Calendar.DATE, 1);
+        }
+
+        System.out.println("A data de vencimento da conta é : "+dataHoje.getTime());
+    }
+}
